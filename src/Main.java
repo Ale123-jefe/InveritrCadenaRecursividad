f435f4ffcef40620faf819472eac4b95df803ae6
Ale123-jefe <83024502+Ale123-jefe@users.noreply.github.com>
2021-09-22 08:49:46 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //.out.println(Ejercicios.factorial(5));
+
+        //System.out.println(Ejercicios.mcd(412, 184));
+        //int[] vector = {4, 4, 6};
+        //System.out.println(Ejercicios.sumaVector(vector, 0));
+        String palabra = "Hola";
+        System.out.println(Ejercicios.invertirPalabra(palabra, palabra.length()-1));
+    }
+}
